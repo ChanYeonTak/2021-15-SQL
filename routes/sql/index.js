697101d8a37277fa883c803177fa226b665049b2
ChanYeonTak <rhymeconqueror@gmail.com>
2021-09-02 14:41:37 +0900

    라우터 테스트 완료

--- routes/sql/index.js
+++ routes/sql/index.js
@@ -2,34 +2,23 @@ const path = require('path')
 const express = require('express')
 const router = express.Router()
 const { error } = require('../../modules/util')
+const { pool } = require('../../modules/mysql')
 
-const mysql = require('mysql2')
-
-const connection = mysql.createConnection({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME 
+router.get('/list', async (req, res, next) => {
+	let sql = 'SELECT * FROM books'
+	let r = await pool.execute(sql)
+	res.status(200).json(r)
 })
 
-router.get('/list', (req, res, next) => { 
-  let sql = 'SELECT * FROM books'
-  const onResult = (err, r) => {
-    res.status(200).json(r)
-  }
-  connection.query(sql, onResult)
-})
-
-router.get('/create', (req, res, next) => {
-  let title = "모든 경계에는 꽃이 핀다"
-  let writer = '함민복'
-  let content = '말랑말랑한 힘'
-  let sql = `INSERT INTO books SET title='${title}', writer='${writer}', content='${content}'`
-  const onResult = (err, r) => {
-    res.status(200).json(r)
-  }
-  connection.query(sql, onResult)
+router.get('/create', async (req, res, next) => {
+	let title = '모든 경계에는 꽃이 핀다'
+	let writer = '함민복'
+	let content = '말랑말랑한 힘'
+	//let sql = `INSERT INTO books SET title='${title}', writer='${writer}', content='${content}'`
+	let sql = 'INSERT INTO books SET title=?, writer=?, content=?'
+	let values = [title, writer, content]
+  let r = await pool.execute(sql, values)
+	res.status(200).json(r)
 })
 
 module.exports = router
