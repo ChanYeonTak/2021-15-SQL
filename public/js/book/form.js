3c36db56e1850210fd68702cc131f0de4cffdfee
ChanYeonTak <rhymeconqueror@gmail.com>
2021-09-14 12:23:54 +0900

    frontend 삭제 작업중

--- public/js/book/form.js
+++ public/js/book/form.js
@@ -6,8 +6,27 @@ function onSubmit(e) {
 	var content = this.content.value.trim();
 	if(!title) {
 		alert('도서명을 입력하세요.');
-		this.title.focus();
+		this.title.focus();ㄴ
 		return false;
 	}
 	this.submit();
 }
+
+document.querySelector('#btRemoveCover').addEventListener('click', onRemoveFile)
+document.querySelector('#btRemoveFile').addEventListener('click', onRemoveFile)
+
+function onRemoveFile(e) {
+	var idx = this.dataset['idx'];
+	var lang = this.dataset['lang'];
+	var parent = this.parentNode;
+	axios.delete('/'+lang+'/book/api/file', { params : { idx: idx } }).then(onSuccess).catch(onError);
+	
+	function onSuccess(r) {
+
+	}
+	
+	function onError(err) {
+		
+	}
+}
+
