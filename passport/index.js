21cff64a6f5c4ec337dc59349c89dadaf0cfd1d5
ChanYeonTak <rhymeconqueror@gmail.com>
2021-09-29 14:43:18 +0900

    kakao stratejy 완료

--- passport/index.js
+++ passport/index.js
@@ -1,3 +1,14 @@
+/*
+1. 로그인 -> serialize : browser 의 cookie에 idx를 남김
+2. 재접속 -> deserialize -> req.user
+3. strategy logic 
+  가. 로그인 라우터에 passport.authenticate([ 'local', 'kakao', 'naver' ]) 통과
+  나. 각 strategy로 이동 
+  다. local 은 done() 
+  라. kakao/naver 는 passport-kakao(naver)가 done을 내장하고 있으므로, 미들웨어로만 진행
+*/
+
+
 const local = require('./local-strategy')
 const kakao = require('./kakao-strategy')
 const { findUser } = require('../models/auth')
--- routes/auth/kakao-router.js
+++ routes/auth/kakao-router.js
@@ -2,14 +2,14 @@ const path = require('path')
 const passport = require('passport')
 const express = require('express')
 const router = express.Router()
-const { error } = require('../../modules/util')
+const { error, alert } = require('../../modules/util')
 
 router.get('/', passport.authenticate('kakao'))
 
 router.get('/cb', passport.authenticate('kakao',
    {failureRedirect: '/'}), 
    (req, res, next) => {
-	res.redirect('/')
+	res.send(alert('로그인 되었습니다.'))
 })
 
 module.exports = router
